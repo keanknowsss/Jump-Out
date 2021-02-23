d39a9f42d0cf8d7ea92f58a5b72a9281cdc01e53
mnslnln <72682339+mnslnln@users.noreply.github.com>
2021-02-23 19:47:36 +0800

    Update MenuState.lua

--- src/states/MenuState.lua
+++ src/states/MenuState.lua
@@ -1,36 +1,50 @@
 MenuState = Class{__includes = BaseState}
 
-function MenuState:init()
-
-end
-
 function MenuState:update(dt)
+     bgMenuScroll = (bgMenuScroll + bgMenuScroll_Speed * dt) % bgMenuLooping
+    
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('play')
-    elseif love.keyboard.wasPressed('h') then
-        gStateMachine:change('score')
     elseif love.keyboard.wasPressed('escape') then
         love.event.quit()
     elseif love.keyboard.wasPressed('o') then
+        gStateMachine:change('over')
+    end
+    if Button_click(190,160,64,17) and  love.mouse.wasPressed(1) then
+        gStateMachine:change('play')
+    elseif Button_click(10,220,100,10) and  love.mouse.wasPressed(1) then
+        gStateMachine:change('scores')
+    elseif Button_click(380,220,40,10) and  love.mouse.wasPressed(1) then
+        love.event.quit()
+    elseif Button_click(350,10,70,10) and  love.mouse.wasPressed(1) then
         gStateMachine:change('option')
-    elseif love.keyboard.wasPressed('c') then
+    elseif Button_click(350,30,70,10) and  love.mouse.wasPressed(1) then
         gStateMachine:change('credit')
+
     end
 
 end
 
 function MenuState:render()
 
-    love.graphics.setFont(gFonts['huge'])
-    love.graphics.setColor(0, 0, 0, 1)
-    love.graphics.printf('JUMP OUT', 0, 64, VIRTUAL_WIDTH, 'center')
+    love.graphics.draw(gBackgrounds['menubg'], -bgMenuScroll,0)
+    love.graphics.draw(gTextures['jlogo'],187,30)
+    love.graphics.draw(gTextures['title'], 134, 110)
+    Button_draw(1)
+    Button_draw(2)
+    Button_draw(3)
+    Button_draw(4)
+    Button_draw(5)
 
-    love.graphics.setFont(gFonts['medium'])
-    love.graphics.setColor(0, 0, 0, 1)
-    love.graphics.printf('Press Enter to Play', 0, 130, VIRTUAL_WIDTH, 'center')
-    love.graphics.printf('High Score (H)', 10, 200, VIRTUAL_WIDTH, 'left')
-    love.graphics.printf('Exit (Esc)', -10, 200, VIRTUAL_WIDTH, 'right')
-    love.graphics.printf('Options (O)', -10, 10, VIRTUAL_WIDTH, 'right')
-    love.graphics.printf('Credits (C)', -10, 30, VIRTUAL_WIDTH, 'right')
-    
+    if Button_click(190,160,64,17) then
+        love.graphics.draw(gSelect['splay'], 189,159)
+    elseif Button_click(10,220,100,10) then
+        love.graphics.draw(gSelect['sscore'], 9,219)
+    elseif Button_click(380,220,40,10) then
+        love.graphics.draw(gSelect['sexit'], 379,219)
+    elseif Button_click(350,10,70,10) then
+        love.graphics.draw(gSelect['soption'], 349,9)
+    elseif Button_click(350,30,70,10) then
+        love.graphics.draw(gSelect['scredit'], 349,29)
+    end
 end
