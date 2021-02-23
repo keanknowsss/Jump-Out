2638ff3ea18329aad17b60bbc23df8df3687612a
mnslnln <72682339+mnslnln@users.noreply.github.com>
2021-02-23 19:40:23 +0800

    Update CreditState.lua

--- src/states/CreditState.lua
+++ src/states/CreditState.lua
@@ -1,19 +1,24 @@
 CreditState = Class{__includes = BaseState}
 
-function CreditState:init()
-    
-end
-
 function CreditState:update(dt)
+    bgMenuScroll = (bgMenuScroll + bgMenuScroll_Speed * dt) % bgMenuLooping
     if love.keyboard.wasPressed('escape') then
+        gStateMachine:change('menu')
+    end
+    
+    if Button_click(380,200,40,10) and  love.mouse.wasPressed(1) then
         gStateMachine:change('menu')
     end
 end
 
 function CreditState:render()
+    love.graphics.draw(gBackgrounds['menubg'], -bgMenuScroll,0)
+
     love.graphics.draw(gTextures['glogo'], VIRTUAL_WIDTH/2 -50,20)
-    love.graphics.setFont(gFonts['medium'])
-    love.graphics.setColor(0, 0, 0, 1)
-    love.graphics.printf('Back (Esc)', -10, 200, VIRTUAL_WIDTH, 'right')
-    
+    love.graphics.draw(gTextures['jlogo'],VIRTUAL_WIDTH/2 - 35 ,130)
+
+    Button_draw(6)
+    if Button_click(380,200,40,10) then
+        love.graphics.draw(gSelect['sback'], 379,199)
+    end
 end
